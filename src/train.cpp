79bbb38a5d9b29a2313e9aabf58a8eb48a733c6e
Jason Huang <jason5528460@gmail.com>
2021-11-08 11:57:04 +0800

    finish gamma/epsilon implementation

--- src/train.cpp
+++ src/train.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 typedef vector<vector<double>> vvb;
+typedef vector<vector<vector<double>>> vvvb;
 
 void calc_alpha(HMM& hmm, vvb& alpha, string& seq) {
     int seq_len = seq.size();
@@ -42,9 +43,47 @@ void calc_beta(HMM& hmm, vvb& beta, string& seq) {
             for (int k = 0; k < hmm.state_num; ++k) {
                 // i := current sequence index
                 // j := current state
-                // k := previous state
-                beta[i][j] += (beta[i + 1][k] * hmm.transition[k][j] *
+                // k := next state
+                beta[i][j] += (beta[i + 1][k] * hmm.transition[j][k] *
                                hmm.observation[seq[i]][j]);
+            }
+        }
+    }
+}
+
+void calc_gamma(HMM& hmm, vvb& alpha, vvb& beta, vvb& gamma, int seq_len) {
+    for (int i = 0; i < seq_len; ++i) {
+        double sum = 0.0;
+        for (int j = 0; j < hmm.state_num; ++j) {
+            // i := current sequence index
+            // j := current state
+            sum += (alpha[i][j] * beta[i][j]);
+        }
+        for (int j = 0; j < hmm.state_num; ++j) {
+            gamma[i][j] = (alpha[i][j] * beta[i][j]) / sum;
+        }
+    }
+}
+
+void calc_epsilon(HMM& hmm, vvb& alpha, vvb& beta, vvvb& epsilon, string& seq) {
+    int seq_len = seq.size();
+    for (int i = 0; i < seq_len - 1; ++i) {
+        double sum = 0.0;
+        for (int j = 0; j < hmm.state_num; ++j) {
+            for (int k = 0; k < hmm.state_num; ++k) {
+                // i := current sequence index
+                // j := current state
+                // k := next state
+                sum += (alpha[i][j] * hmm.transition[j][k] *
+                        hmm.observation[seq[i + 1]][k] * beta[i + 1][k]);
+            }
+        }
+        for (int j = 0; j < hmm.state_num; ++j) {
+            for (int k = 0; k < hmm.state_num; ++k) {
+                epsilon[i][j][k] =
+                    (alpha[i][j] * hmm.transition[j][k] *
+                     hmm.observation[seq[i + 1]][k] * beta[i + 1][k]) /
+                    sum;
             }
         }
     }
